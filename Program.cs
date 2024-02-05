2a60333a8eeefa74bec34972bedd30c6fccebab8
kawan <kawachaudhary@gmail.com>
2024-02-05 19:07:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LowLevelDesign
+{
+    class LowLevelDesign
+    {
+        static void Main()
+        {
+            Console.WriteLine("--------Welcome to low level design-------");
+        }
+    }
+}
